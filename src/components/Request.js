9246518af370204e9350865bb58b81798016b86a
AryusG <verdyg24@gmail.com>
2022-06-13 00:14:59 +1000

    successful-map-of-users

--- src/components/Request.js
+++ src/components/Request.js
@@ -1,29 +1,19 @@
 import React, { useState, useEffect } from 'react'
 
-const url = "https://randomuser.me/api/";
+const url = "https://randomuser.me/api/?results=5";
 const err_url = "https://httpstat.us/400";
 
 function Request() {
   const [loading, setLoading] = useState(true);
-  const [person, setPerson] = useState({
-    firstName: "",
-    lastName: "", 
-    email: "",
-    picture: {},
-  })
+  const [people, setPeople] = useState([])
   
   const fetchUser = async () => {
     setLoading(true);
     const res = await fetch(url);
     const data = await res.json();
-    const data_cleaned = data.results[0];
-    setPerson({
-      ...person,
-      firstName: data_cleaned.name.first,
-      lastName: data_cleaned.name.last,
-      email: data_cleaned.email,
-      picture: data_cleaned.picture,
-    })
+    const data_cleaned = data.results;
+    setPeople(data_cleaned)
+    console.log(people)
     setLoading(false);
   }
 
@@ -40,13 +30,19 @@ function Request() {
     )
   }
 
+  if (!people.length) {
+    return (
+      <div>no people...</div>
+    )
+  }
+
   return (
     <div>
-      <h1>
-        {person.firstName} {person.lastName}
+      {/* <h1>
+        {people.firstName} {people.lastName}
       </h1> 
-      <img src={person.picture.large} />
-      <p>{person.email}</p>
+      <img src={people.picture.large} />
+      <p>{people.email}</p> */}
       <button onClick={fetchUser}>Random</button>
     </div>
   )
